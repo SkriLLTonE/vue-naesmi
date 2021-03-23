dc8545ff6cffc3d90479a8acc368bcd4c0d8518d
Пользователь <timurkazah1998@gmail.com>
2021-03-23 16:17:42 +0500

    change about and enother deleted block

--- src/views/About.vue
+++ src/views/About.vue
@@ -28,16 +28,6 @@ O‘zEOAVMA faoliyati yo‘nalishlaridan yana biri xalqaro ekspertlar ishtirokid
           <span style="font-weight: bold;font-size: 32px;">A’zolar:</span> <br><br>
          Bugungi kunda O‘zEOAVMA 18 ta milliy telekanal, 19 ta radiokanal, O‘zbekiston kabel televideniyesiga a’zo bo‘lgan 100 ta elektron OAV (shundan 52 tasi xorijiy kanallar)ni o‘z ichiga oladi. O‘zEOAVMA a’zolarit orasida mamlakatimizdav Yangi O‘zbekiston sharoitida OAV rivoi uchun yaratilgan ulkan imkoniyatlar tufayli yuqori reytingga ega bo‘lgan, Sevimli TV, ZOR TV, MY5 va Uzreport TV singari yetakchi telekanallar bor. Umuman, barcha teletomoshabinlarning 76%i ushbu kanallarga ishonch bildirishgan.
 O‘zEOAVMA xalqaro miqyosdagi hamkorlikni rivojlantirishga ham katta e’tibor qaratmoqda. O‘zEOAVMAning xorijiy va xalqaro tashkilotlar, madaniy markazlar, turli mamlakatlarning elchixonalari va konsulxonalari, yirik media-tuzilmalar bilan o‘zaro hamkorligi va hamjihatligini ta’minlash maqsadlari birinchi navbatda xorijlik murabbiylarni milliy media-maydonga jalb qilish, yosh ijodiy va texnik xodimlarning malakasini oshirish, kontent almashish, hamkorlikdagi media-loyihalarni amalga oshirishga yo‘naltirilgan. <br><br>
-
-          <span style="font-weight: bold;font-size: 32px;">Loyihalar: </span> <br><br>
-       9 oy davomida 80 dan ortiq axborot nashrlari tayyorlandi
-"mening yurtim" telekanali uchun "O'zbekiston-Online" dasturi va har birida
-dastur Reuters dan 10 dan ortiq yangiliklar hisobotlar uzatiladi. Bundan tashqari
-Togo "Xorij xabarlari" dasturining 125 dan ortiq nashri tayyorlandi"
-("Xorijiy yangiliklar") 750 yangiliklari efirga uzatildi
-Reuters tarkib foydalanish bilan.Bundan tashqari, 540 tayyorlandi
-"zamon 120" dasturining chiqishi, bu erda 1595 xorijiy yangiliklar efirga uzatildi
-Reuters tomonidan.<br><br>
            <span style="font-weight: bold;font-size: 32px;">Xalqaro hamkorlik:</span>
           <br>
           <br>
@@ -47,23 +37,7 @@ Shuningdek, UNESCOning O‘zbekistondagi vakolatxonasi, O‘zEOAVMA va BBC hamko
 Bundan tashqari, “Rossiya-Kultura” telekanali bilan hamkorlikda “Nukus. Noma’lum kolleksiya”, “O‘zbekiston. Kashf etilgan yangiliklar”, “O‘zbekiston. Sevgi haqida afsonalar”, “O‘zbekiston. Vaqt qumlari oralab” kabi bir qator hujjatli filmlar yaratildi. Ayni paytda rossiyalik hamkorlar ko‘magida yana bir qator yangi filmlar suratga olish ishlari boshlangan.
 Ayni paytda yevropalik mutaxassislar bilan ham hamkorlik ishlari davom etmoqda. Xususan, Gete institutining O‘zbekistondagi vakolatxonasi bilan hamkorlikda Berlindagi fonogrammalar arxivida saqlanayotgan O‘zbekiston musiqiy merosi namunalarini o‘rgarish va raqamlashtirish, ushbu mavzuda kitob-albomlar chop etish ishlari ham samarali yo‘lga qo‘yildi.
 Shuningdek, Turkiyaning TRT teleradiokompaniyasi bilan ham ijodiy aloqalar yo‘lga qo‘yilgan. Ayni paytda turkiyalik hamkorlar bilan birgalikda buyuk ajdodlarimiz merosi, ularning jahon tamadduniga qo‘shgan hissasiga bag‘ishlangan qo‘shma loyiha ustida ish olib borilmoqda.
-Ayni paytda Germaniyaning “Deutsche Welle” (“Nemis to‘lqinim”) media-brendli bilan hamkorlik munosabatlarini o‘rnatish bo‘yicha muzokaralar davom etmoqda.<br> <br>
-
-          <span style="font-weight: bold;font-size: 32px;">Yangi loyihalar:</span>
-
-          <br><br>
-      13-aprel kuni "sevimli" telekanalining ofisida bo'lib o'tdi
-Gyote instituti vakili Yan Helfer bilan uchrashuv
-qo'shma multimedia loyihasini amalga oshirish tashabbusi ilgari surildi,
-birinchi noyob musiqalarni o'rganish va raqamlashtirishga qaratilgan
-hozirgi vaqtda Berlinning fonogrammarhiv shahrida saqlanadigan yozuvlar
-shuningdek, ushbu mavzuga bag'ishlangan kitob yaratish. Bir necha kun
-bundan oldin, Janob Avaz Sedat Sagyrkaya bilan uchrashuvimiz davomida –
-Turkiya TV kompaniyasi TRT rahbari, biz amalga oshirish uchun kelishib oldilar
-bizning bebaho hissa qo'shgan buyuk ajdodlarimiz haqida loyiha
-jahon sivilizatsiyasining rivojlanishi. Yaqin istiqbollardan – tashkil etish
-"Doyche Velle" global media brendi bilan kontentni almashish bo'yicha aloqalar
-biz hozirda hamkorlik ostonasida turibmiz.</span>
+            Ayni paytda Germaniyaning “Deutsche Welle” (“Nemis to‘lqinim”) media-brendli bilan hamkorlik munosabatlarini o‘rnatish bo‘yicha muzokaralar davom etmoqda.<br> <br></span>
             </div>
         </div>
     </div>
